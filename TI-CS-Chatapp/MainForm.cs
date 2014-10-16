6565a17bfbc68106929daba94c7cb31170ef498b
Bart Reedijk <bart@mrdk.nl>
2014-10-16 10:15:10 +0200

    change name of Form1 to MainForm

---
+++ TI-CS-Chatapp/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TI_CS_Chatapp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
