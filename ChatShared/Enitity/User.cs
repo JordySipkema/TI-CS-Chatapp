7ee383d2b3f9e3e74c462ffb125e3ff4486b9e34
Bart Reedijk <bart@mrdk.nl>
2014-10-23 11:20:18 +0200

    Project: Chatshared: added User.cs

---
+++ ChatShared/Enitity/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatShared.Enitity
+{
+    public class User
+    {
+        public string Nickname { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+
+        public List<ChatMessage> Messages { get; set; }
+        
+        public User(string nickname, string username, string password)
+        {
+            this.Nickname = nickname;
+            this.Username = username;
+            this.Password = password;
+        }
+
+        public void changeNickname(string nickname)
+        {
+            this.Nickname = nickname;
+        }
+
+        public void ChangePassword()
+        {
+
+
+        }
+
+    }
+}
