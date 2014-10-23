d32c459cfa2d9ab0455f0213fa87a548a6cfbbc4
JordySipkema <jordysipkema@gmail.com>
2014-10-23 12:12:49 +0200

    Chatserver: Create stub for datastorage
    Signed-off-by: JordySipkema <jordysipkema@gmail.com>

--- Chatserver/FileController/Datastorage.cs
+++ Chatserver/FileController/Datastorage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using ChatShared.Entity;
 using ChatShared.Utilities;
 
@@ -12,14 +13,30 @@ namespace Chatserver.FileController
             get { return _instance ?? (_instance = new Datastorage()); }
         }
 
+        private readonly List<User> _users = new List<User>();
+        private readonly List<ChatMessage> _messages = new List<ChatMessage>(); 
+
         private Datastorage()
         {
+            _users.Add(new User("Henk", "testuser01", Crypto.CreateSHA256("1234")));
+
             // TODO: Create initializer code (fetching data from FileIO?)
         } 
 
         public User GetUser(string username)
         {
-            return new User("Henk", "testuser01", Crypto.CreateSHA256("1234"));
+            return _users.FirstOrDefault(user => user.Username == username);
+        }
+
+        public IEnumerable<ChatMessage> GetMessages(string username)
+        {
+            var x =
+                from message in _messages
+                where message.Recipient == username || message.Sender == username
+                orderby message.Timestamp ascending
+                select message;
+
+            return x;
         }
     }
 }
