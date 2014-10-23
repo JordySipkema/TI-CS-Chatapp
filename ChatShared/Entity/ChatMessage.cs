8127542e0a73672fe56f46124ed8f5c9ea73fbfa
JordySipkema <jordysipkema@gmail.com>
2014-10-23 11:19:59 +0200

    ChatShared>Entity: Add ChatMessage Class
    Signed-off-by: JordySipkema <jordysipkema@gmail.com>

---
+++ ChatShared/Entity/ChatMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatShared.Entity
+{
+    public class ChatMessage
+    {
+        public String Sender { get; private set; }
+        public String Recipient { get; private set; }
+        public String Message { get; private set; }
+        public DateTime Timestamp { get; private set; }
+
+        public ChatMessage(string sender, string recipient, string message, DateTime timestamp)
+        {
+            Sender = sender;
+            Recipient = recipient;
+            Message = message;
+            Timestamp = timestamp;
+        }
+
+
+
+
+    }
+}
